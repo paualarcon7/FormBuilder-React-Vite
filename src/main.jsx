3ecb99143d16327adb3103028ca8aa60e12e5769
paualarcon7 <alarconayelenpaulina@gmail.com>
2023-03-30 13:21:20 -0300

    (Form & FormData): correcting styles

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.render(
   <ChakraProvider>
 
     <React.StrictMode>
-    <App />
+    <App bgImage="url('https://res.cloudinary.com/djobvomhs/image/upload/v1680190694/FormBuilder/fondoflores_hus9us.jpg')" minHeight="100vh"/>
     
   </React.StrictMode>
   </ChakraProvider>
